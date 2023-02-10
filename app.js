58849d262b7eb99511be30cbbcc0d8e3f4f9fe8f
zepower72 <114823832+zepower72@users.noreply.github.com>
2023-02-10 14:36:41 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const path = require("path");
+require("dotenv").config();
+
+const userRoutes = require("./routes/user");
+const sauceRoutes = require("./routes/sauce");
+
+console.log(process.env.SECRET_DB);
+mongoose
+  .connect(process.env.SECRET_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) => console.log(error));
+
+const app = express();
+
+app.use(helmet());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(express.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", sauceRoutes);
+
+module.exports = app;
---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router()
+
+const userCtrl = require("../controllers/user");
+const checkPassword = require("../middleware/check-password")
+const checkEmail = require("../middleware/check-email")
+
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
+
+module.exports = router;
